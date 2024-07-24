459cca385cb384105d635520b18d6a6523bb065f
brogli <alexanderbrogli@gmx.net>
2024-07-24 16:51:06 +0200

    saving of measurements to store now possible

--- src/components/CreateMeasurement.vue
+++ src/components/CreateMeasurement.vue
@@ -1,40 +1,63 @@
 <script setup lang="ts">
 
-import { ref } from "vue";
-import InputNumber from 'primevue/inputnumber';
-import SelectButton from 'primevue/selectbutton';
+import { ref } from 'vue'
+import InputNumber from 'primevue/inputnumber'
+import SelectButton from 'primevue/selectbutton'
+import { type ArmOption, Measurement } from '@/models/Measurement'
+import Button from 'primevue/button'
+import { useMeasurementsStore } from '@/stores/measurements'
+import { useRouter } from 'vue-router'
 
-const systolic = ref(42723);
-const diastolic = ref(2351.35);
-const heartRate = ref(50);
+const measurements = useMeasurementsStore()
+const router = useRouter();
 
-const armSelectionValue = ref('Left');
-const armSelectionOptions = ref(['Left', 'Right']);
+const systolic = ref(120)
+const diastolic = ref(80)
+const heartRate = ref(70)
+
+const armSelectionValue = ref<ArmOption>('Left')
+const armSelectionOptions = ref<ArmOption[]>(['Left', 'Right'])
+
+function handleClick() {
+  saveMeasurements()
+}
+
+
+function saveMeasurements() {
+  const measurement = new Measurement(new Date(), systolic.value, diastolic.value, heartRate.value, armSelectionValue.value)
+  measurements.addMeasurement(measurement)
+  router.push("/")
+}
 
 </script>
 
 <template>
   <div class="bp-form">
-    <div class="">
-      <label for="systolic" class="font-bold"> Systolic </label>
-      <InputNumber v-model="systolic" inputId="integeronly" fluid />
+    <div class="bp-form-inputs">
+      <div class="">
+        <label for="systolic" class="font-bold"> Systolic </label>
+        <InputNumber v-model="systolic" inputId="integeronly" fluid />
+      </div>
+      <div class="">
+        <label for="diastolic" class="font-bold"> Diastolic </label>
+        <InputNumber v-model="diastolic" inputId="diastolic" fluid />
+      </div>
+      <div class="">
+        <label for="heartrate" class="font-bold"> Heart Rate </label>
+        <InputNumber v-model="heartRate" inputId="heartrate" fluid />
+      </div>
+      <div class="">
+        <label for="armSelection" class="font-bold">Which arm?</label>
+        <SelectButton inputId="armSelection" v-model="armSelectionValue" :options="armSelectionOptions"
+                      aria-labelledby="basic" />
+      </div>
     </div>
-    <div class="">
-      <label for="diastolic" class="font-bold"> Diastolic </label>
-      <InputNumber v-model="diastolic" inputId="diastolic" fluid />
-    </div>
-    <div class="">
-      <label for="heartrate" class="font-bold"> Heart Rate </label>
-      <InputNumber v-model="heartRate" inputId="heartrate" fluid />
-    </div>
-    <div class="">
-      <label for="armSelection" class="font-bold"> Heart Rate </label>
-      <SelectButton inputId="armSelection" v-model="armSelectionValue" :options="armSelectionOptions" aria-labelledby="basic" />
+    <div class="bp-form-buttons">
+      <Button label="Save" @click="handleClick" />
+      <Button label="Cancel" severity="secondary" as="router-link" to="/" />
     </div>
   </div>
-  <div>
 
-  </div>
 </template>
 
 <style scoped>
--- src/stores/measurements.ts
+++ src/stores/measurements.ts
@@ -1,4 +1,4 @@
-import { ref, computed, type Ref } from 'vue'
+import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { Measurement } from '@/models/Measurement'
 
