49e5ea00fc7a5c34087a48aefe70de743898a3a1
brogli <alexanderbrogli@gmx.net>
2024-08-01 02:05:53 +0200

    write and read to local storage manually and resolve deserialization issues

--- src/stores/measurements.ts
+++ src/stores/measurements.ts
@@ -1,16 +1,40 @@
 import { computed, type ComputedRef, ref, type Ref } from "vue";
 import { defineStore } from "pinia";
-import type { Measurement } from "@/models/Measurement";
+import { DateWrapper } from "@/models/DateWrapper";
+import { Measurement } from "@/models/Measurement";
 
 export const useMeasurementsStore = defineStore("measurements", () => {
+  const localStorageKeyName = "local";
   const state: Ref<Map<string, Measurement>> = ref(new Map<string, Measurement>());
 
   function saveMeasurement(measurement: Measurement) {
     state.value.set(measurement.id, measurement);
+
+    localStorage.setItem(localStorageKeyName, JSON.stringify(Array.from(state.value.values())));
   }
 
   function clearMeasurements() {
     state.value.clear();
+
+    localStorage.clear();
+  }
+
+  function loadFromLocalStorage(localStorageContent) {
+    const objects: any[] = JSON.parse(localStorageContent);
+    objects.forEach((object) => {
+      const pseudoMeasurement = object as Measurement;
+      const nativeTimeStamp = new Date(pseudoMeasurement.timestamp.nativeTimeStamp);
+      const properDateWrapper = new DateWrapper(nativeTimeStamp);
+      const properMeasurement = new Measurement(
+        properDateWrapper,
+        pseudoMeasurement.systolic,
+        pseudoMeasurement.diastolic,
+        pseudoMeasurement.heartRate,
+        pseudoMeasurement.whichArm,
+        pseudoMeasurement.id,
+      );
+      saveMeasurement(properMeasurement);
+    });
   }
 
   const getAllMeasurements: ComputedRef<Measurement[]> = computed(() => Array.from(state.value.values()));
@@ -19,5 +43,9 @@ export const useMeasurementsStore = defineStore("measurements", () => {
 
   const size: ComputedRef<number> = computed(() => state.value.size);
 
+  const localStorageContent = localStorage.getItem(localStorageKeyName);
+  if (localStorageContent !== null && localStorageContent.length > 2) {
+    loadFromLocalStorage(localStorageContent);
+  }
   return { size, getAllMeasurements, saveMeasurement, clearMeasurements, getMeasurement };
 });
