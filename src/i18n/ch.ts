24afe7ed4bc73648e028635ba8ff1e8d3c4438a3
brogli <alexanderbrogli@gmx.net>
2024-08-11 11:24:46 +0200

    feature: translate texts into swiss german

--- src/i18n/ch.ts
+++ src/i18n/ch.ts
@@ -40,26 +40,27 @@ export const ch = {
       "D App speicheret dini Mässigä im Local Storage vo dim Browser. Das bedütet, wennt d App ufem gliche Grät im gliche Browser nomel uftuesch spöter, denn söttet d Mässigä immerno existiere und chasch witeri erfasse.",
     dataP2:
       "Wennt d'App imene andere Browser oder ufeme andere Grät öffnisch, existieret dini Mässigä döt nöd. Wennt de Private Mode verwendisch, oder de Cache vo dim Browser löschisch, denn werdet Date nöd gspeicheret. Vermuetli gits no meh so Fäll wie die.",
-    dataP3: "Es ist daher empfohlen, die Export und Import Funktion zu nutzen, um ein Backup der Daten zu erhalten.",
-    dataP4: "Du bist verantwortlich deine Backups sicher zu verwalten.",
+    dataP3:
+      "Es isch drum empfohle, d Export und Import Funktione z'nutze, zum es Backup vode Date exportiere und z'importiere, falls nötig.",
+    dataP4: "Du bist verantwortlich defür dini Backup-Files sicher z verwalte.",
     feedbackTitle: "Feedback",
-    feedback: "Gerne kannst du uns Feedback geben, in dem du ein Github Issue eröffnest: ",
+    feedback: "Chasch gern Feedback geh, indemt es Github Issue eröffnisch: ",
     openSauceTitle: "Open Source",
-    openSauce1: "Der Quellcode ist offen, du findest ihn auf ",
-    openSauce2: "Gerne kannst du lesen, Feedback geben, contributen, forken, oder was auch immer.",
+    openSauce1: "Der Quellcode isch offen, du findschen uf ",
+    openSauce2: "Gerne chasch go läse, Feedback gegeh, contributä, forkä, oder was auch immer.",
     techInfosTitle: "Tech Infos",
     techInfos:
-      "Diese App ist in Vue3 geschrieben, nutzt die Composition API, Pinia, Typescript und Primevue. Deployed ist sie auf Cloudflare Pages.",
+      "Die App isch in Vue3 gschribe, nutzt d'Composition API, Pinia, Typescript und Primevue. Deployt isch si uf  App ist in Vue3 geschrieben, nutzt die Composition API, Pinia, Typescript und Primevue. Deployed ist sie auf Cloudflare Pages.",
   },
   consent: {
-    intro: "Siehst so aus, als nutzt du die App das erste Mal. Mit dem Weiternutzen akzeptierst du, dass:",
-    appLivesInBrowser: "die App nur in deinem Browser existiert",
-    doesntTransmitData: "die App keine Daten ins Internet übermittelt",
-    localStorageConsent: "die App Daten im Local Storage deines Browsers speichern wird",
+    intro: "Gseht so us, als benutzisch die App s erste Mol. Mitem Witerbenutze akzeptiersch, dass:",
+    appLivesInBrowser: "d'App nur in dim Browser existiert",
+    doesntTransmitData: "d'App kei Daten ins Internet schickt",
+    localStorageConsent: "d'App Daten im Local Storage vo dim Browsers wird speichere",
     persistingLimited:
-      "obwohl die App daten in deinem Browser persistiert und wieder lädt, wenn du die App später wieder öffnest, hat diese Funktionalität Limtien",
+      "obwohl d'App Date i dim Browser persistiert und wieder ladt, wenn d'App spöter wieder öffnisch, hät die Funktionalität ihri Limit",
     responsibleForBackups:
-      "deshalb bist du dafür verantwortlich mit Hilfe der Export/Import Funktion Backups deiner Daten zu erstellen, und diese sicher aufzubewahren",
-    noGuarantees: "die App kann Fehler beinhalten, es gibt keine Garantie für irgend etwas",
+      "drum bisch selber defür verantwortlich mit der Export/Import Funktion Backups vo dine Date z'mache, und die sicher ufzbewahre",
+    noGuarantees: "d'App cha fehlerhaft si, es git kei Garantie für irgend öppis",
   },
 };
